0b8919039a0e2a4f10dc39c1531aed832b375aa3
asbsoft <ab2014box@gmail.com>
2017-04-08 13:04:04 +0300

    routes fix if use URL-normalizer

--- config/routes-main.php
+++ config/routes-main.php
@@ -1,7 +1,19 @@
 <?php
+
 // route without prefix => controller/action without current (and parent) module(s) IDs
-return [
+$result = [
     'form'    => 'main/form',
     'captcha' => 'main/captcha',
-    '?'       => 'main/index',
+  //'?'       => 'main/index', // without URL-normalizer
+  //''        => 'main/index', // with URL-normalizer
 ];
+
+$mgr = Yii::$app->urlManager;
+$normalizeTrailingSlash = !empty($mgr->normalizer->normalizeTrailingSlash) && $mgr->normalizer->normalizeTrailingSlash;//var_dump($normalizeTrailingSlash);
+if ($normalizeTrailingSlash) {
+    $result[''] = 'main/index';
+} else {
+    $result['?'] = 'main/index';
+}//var_dump($result);exit;
+
+return $result;
