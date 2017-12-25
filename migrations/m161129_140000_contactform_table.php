573e01b3112304ee7e62e52cd946e81ab4b4ad9e
asbsoft <ab2014box@gmail.com>
2017-12-25 21:31:04 +0200

    update

--- migrations/m161129_140000_contactform_table.php
+++ migrations/m161129_140000_contactform_table.php
@@ -6,7 +6,6 @@ use asb\yii2\modules\contactform_3_170124\models\Contactform as Model;
 
 use yii\db\Schema;
 use yii\db\Migration;
-use yii\db\Expression;
 
 /**
  * @author ASB <ab2014box@gmail.com>
