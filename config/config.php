870ae507feb8926b53bbeed162017bc5e3acae4a
asbsoft <ab2014box@gmail.com>
2018-01-15 17:03:11 +0200

    fix config to properly work in backend/frontend/basic applications

--- config/config.php
+++ config/config.php
@@ -6,27 +6,26 @@
     //use yii\rest\UrlRule as RestUrlRule;
     use asb\yii2\modules\contactform_3_170124\helpers\RestUrlRule;
 
+
+    $adminUrlPrefix = empty(Yii::$app->params['adminPath']) ? '' : Yii::$app->params['adminPath'] . '/';
+    $type = empty(Yii::$app->type) ? false : Yii::$app->type;
+    
     /** Default module config */
     return [
-        //'params' => include(__DIR__ . '/params.php'),
-
         'routesConfig' => [ // type => prefix | array(config)
-            'main'  => [
+            'main' => $type == UniApplication::APP_TYPE_BACKEND ? false : [
                 'urlPrefix'      => 'contactform',
                 'startLinkLabel' => 'Contact us',
             ],
-            'admin' =>  [
-                'urlPrefix' => AdminController::$adminPath === false
-                                 ? false
-                                 : (AdminController::$adminPath === '' ? '' : (AdminController::$adminPath . '/'))
-                                   . 'contactform-back',
+            'admin' => $type == UniApplication::APP_TYPE_FRONTEND ? false : [
+                'urlPrefix' => $adminUrlPrefix . 'contactform-back',
                 'startLink' => [
                     'label' => 'Contactform messages v3',
                   //'link'  => '', // default
                     'action' => 'admin/index',
                 ],
             ],
-            'rest'  => [
+            'rest' => [
                 'class' => RestUrlRule::className(),
                 'urlPrefix' => 'contactform-api',
                 'sublink'   => 'contact-msg',
