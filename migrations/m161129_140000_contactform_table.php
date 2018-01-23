333c2463aac2fd99567a5119edbeddb05ab47b3c
asbsoft <ab2014box@gmail.com>
2018-01-23 14:39:16 +0200

    fix migration script

--- migrations/m161129_140000_contactform_table.php
+++ migrations/m161129_140000_contactform_table.php
@@ -19,7 +19,7 @@ class m161129_140000_contactform_table extends Migration
     {
         parent::init();
 
-        Yii::setAlias('@asb/yii2/modules', '@vendor/asbsoft/yii2module');
+        //Yii::setAlias('@asb/yii2/modules', '@vendor/asbsoft/yii2module');
 
         $this->tableName = Model::tableName();
         $this->idxNamePrefix = 'idx-' . $this->tableName;
